1fcb61ed96c23aba3003b608034f84db2a86eef9
jeemyeong <jeemyeong@gmail.com>
2018-02-25 15:55:38 +0900

    Init bean_practice

---
+++ bean_practice/src/main/java/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Jeemyeong.
+ * User: jeemyeonglee
+ * Date: 25/02/2018
+ * Time: 3:30 PM
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HELLO");
+    }
+}
